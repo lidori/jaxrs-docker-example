2fec4bf8f25ea49c8832583ece1b40ea10fd9a45
lidori <lidori@users.noreply.github.com>
2020-04-28 11:20:44 -0400

    Update HelloService.java

--- src/main/java/org/forge/test/rest/HelloService.java
+++ src/main/java/org/forge/test/rest/HelloService.java
@@ -13,6 +13,10 @@ import com.mongodb.MongoCredential;
 import com.mongodb.MongoClientOptions;
 import java.util.Arrays;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+
 @Path("/greet")
 public class HelloService {
 
@@ -23,6 +27,9 @@ public class HelloService {
 		MongoClient mongoClient = new MongoClient(uri);
 		mongoClient.getDatabaseNames().forEach(System.out::println);
 		String s = mongoClient.getDatabaseNames().get(0);
+		DB database = mongoClient.getDB("myMongoDb");
+		database.createCollection("customers", null);
+		database.getCollectionNames().forEach(System.out::println);
 		return Response.ok("shira method doGet invoked " + s + ", " + new Date()).build();
 	}
 }
