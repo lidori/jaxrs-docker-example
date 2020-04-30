354fd85a993da56d0a7be5846cc5b57393d038fe
lidori <lidori@users.noreply.github.com>
2020-04-30 13:52:26 -0400

    Create User.java

---
+++ src/main/java/org/forge/test/rest/User.java
@@ -0,0 +1,33 @@
+package org.forge.test.rest;
+
+public class Product {
+
+  String id; // email
+	String username;
+	String image; // url of profile image
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+  
+  public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+}
