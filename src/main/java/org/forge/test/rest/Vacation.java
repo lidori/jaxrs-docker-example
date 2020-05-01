de8a2c0c439f355158b83bf933bd359fbc069350
lidori <lidori@users.noreply.github.com>
2020-05-01 10:17:31 -0400

    Create Vacation.java

---
+++ src/main/java/org/forge/test/rest/Vacation.java
@@ -0,0 +1,42 @@
+package org.forge.test.rest;
+
+public class Vacation {
+
+  String id;
+	String title;
+	String description;
+  String type;
+  
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+  
+  public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+  
+  public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
